1338f6b108afbeff047dede4d89a9b402d445676
Tien An <tienlenan.92@gmail.com>
2021-08-03 13:45:22 +0700

    update buildspec.yaml file

--- lambda-01/hello-world/app.js
+++ lambda-01/hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world Lambda-01-03082021',
+                message: 'hello world Cuong',
                 // location: ret.data.trim()
             })
         }
